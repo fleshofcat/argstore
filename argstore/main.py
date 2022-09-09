314d280aec942c509f53e2995a182c527cb8a7c3
fleshofcat <fleshofcat@gmail.com>
2022-09-09 04:44:45 +0300

    Include params router to app

--- argstore/main.py
+++ argstore/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
+from . import models
+from .database import engine
+from .router import router
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(router, tags=["parameters"])
 
 
 @app.get("/")
