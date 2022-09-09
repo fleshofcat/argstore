f7a34dafbc9c64e606eb3fa6372ad2d9cc927b6b
fleshofcat <fleshofcat@gmail.com>
2022-09-09 04:45:49 +0300

    Fix status code for create parameter

--- tests/parameters/test_router.py
+++ tests/parameters/test_router.py
@@ -2,6 +2,6 @@ from starlette.testclient import TestClient
 
 
 def test_create_parameter(client: TestClient):
-    response = client.post("/parameter", json={"name": "param_name", "value": 20})
-    assert response.ok
+    response = client.post("/parameters", json={"name": "param_name", "value": 20})
+    assert response.status_code == 201, response.reason
     assert False, "The test must check that new param is appeared"
