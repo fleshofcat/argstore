079f2b0627fec670a16939ea76f1550033fe9fe1
fleshofcat <fleshofcat@gmail.com>
2022-09-09 04:45:14 +0300

    Remove client fixture from test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,4 @@
-import pytest
 from starlette.testclient import TestClient
-
-from argstore.main import app
-
-
-@pytest.fixture(scope="session")
-def client() -> TestClient:
-    return TestClient(app)
 
 
 def test_redirect_to_docs_from_root(client: TestClient):
