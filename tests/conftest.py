3ecb200ecda073642304060071b0349f357185e4
fleshofcat <fleshofcat@gmail.com>
2022-09-10 13:08:35 +0300

    Use separate db for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,43 @@
+import os
+
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 from starlette.testclient import TestClient
 
+from argstore.database import Base
 from argstore.main import app
+from argstore.parameters.router import get_db
 
 
 @pytest.fixture(scope="session")
-def client() -> TestClient:
+def use_test_db():
+    db_filename = "./test.db"
+    sqlalchemy_database_url = f"sqlite:///{db_filename}"
+
+    engine = create_engine(
+        sqlalchemy_database_url, connect_args={"check_same_thread": False}
+    )
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    def get_test_db():
+        db: Session = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = get_test_db
+    yield
+    del app.dependency_overrides[get_db]
+
+    # Comment it to save the db with test results
+    os.remove(db_filename)
+
+
+@pytest.fixture(scope="session")
+def client(use_test_db) -> TestClient:
     return TestClient(app)
