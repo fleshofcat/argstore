3e76e1a126084630355bd05eb3064c1b56bd666b
fleshofcat <fleshofcat@gmail.com>
2022-09-11 01:47:03 +0300

    Fix mock db initialization

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,13 +4,32 @@ import pytest
 from pytest_mock import MockerFixture
 from sqlalchemy.orm import Session
 
-from argstore import database
+from argstore import database, settings
 
 
-def test_get_db(mocker: MockerFixture):
+@pytest.fixture
+def db_is_not_initialized():
+    # noinspection PyProtectedMember
+    old_engine = database._engine
 
-    mocker.patch.object(database, "_engine")
-    mocker.patch.object(database, "_SessionFabric")
+    # noinspection PyProtectedMember
+    old_SessionFabric = database._SessionFabric
+
+    database._engine = None
+    database._SessionFabric = None
+
+    yield
+
+    database._engine = old_engine
+    database._SessionFabric = old_SessionFabric
+
+
+def test_get_db(mocker: MockerFixture, db_is_not_initialized):
+    mocker.patch.object(
+        database,
+        "Settings",
+        return_value=settings.Settings(sqlalchemy_database_url="sqlite:///./test.db"),
+    )
 
     session_generator = database.get_db()
     session: Session = next(session_generator)
