a8c04fb7c1d448717caf3da647a0301a11982801
fleshofcat <fleshofcat@gmail.com>
2022-09-10 12:30:19 +0300

    remove unnecessary database creation attempt

--- argstore/database.py
+++ argstore/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import create_database, database_exists
 
 from argstore.settings import Settings
 
@@ -10,9 +9,6 @@ settings = Settings()
 engine = create_engine(
     settings.sqlalchemy_database_url, connect_args={"check_same_thread": False}
 )
-if not database_exists(engine.url):
-    create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
