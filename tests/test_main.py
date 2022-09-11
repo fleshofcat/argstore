c70d27b8c866cbd813caf788e52f78024117a6cb
fleshofcat <fleshofcat@gmail.com>
2022-09-11 21:12:40 +0300

    Make a `client` fixture and a `client_without_db`

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 from starlette.testclient import TestClient
 
 
-def test_redirect_to_docs_from_root(client: TestClient):
-    response = client.get("/")
+def test_redirect_to_docs_from_root(client_without_db: TestClient):
+    response = client_without_db.get("/")
     assert response.ok
     assert response.url.endswith("/docs")
