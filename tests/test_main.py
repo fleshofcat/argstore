fae3bfd433ee3403132f0a4f38adfddb7fce41d1
fleshofcat <fleshofcat@gmail.com>
2022-09-14 15:33:57 +0300

    Fix coverage for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,2 @@
-from starlette.testclient import TestClient
-
-
-def test_redirect_to_docs_from_root(client_without_db: TestClient):
-    response = client_without_db.get("/")
-    assert response.ok
-    assert response.url.endswith("/docs")
+def test_main_imports():
+    from argstore import main  # noqa: F401
