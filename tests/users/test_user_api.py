85927d313b3d0d2176b180c66ec5ad54b83a3e4d
fleshofcat <fleshofcat@gmail.com>
2022-09-14 15:26:01 +0300

    Test read users endpoint

--- tests/users/test_user_api.py
+++ tests/users/test_user_api.py
@@ -6,3 +6,32 @@ def test_create_read_user(client: TestClient):
     assert bob_response.status_code == 201, bob_response.reason
     assert bob_response.json() == {"Name": "Bob", "Parameters": []}
     assert client.get("/api/users/Bob").json() == bob_response.json()
+
+
+def test_read_users(client: TestClient):
+    client.post("/api/users/", json={"Name": "Alice"})
+    client.post("/api/users/", json={"Name": "John"})
+
+    client.post(
+        "/api/parameters/John/param_of_john/str",
+        data="val",
+        headers={"Content-type": "text/plain"},
+    )
+
+    users_response = client.get("/api/users/")
+    assert users_response.status_code == 200
+
+    users = users_response.json()
+
+    assert type(users) is list
+    assert {"Name": "Alice", "Parameters": []} in users
+    assert {
+        "Name": "John",
+        "Parameters": [{"Name": "param_of_john", "Type": "str", "Value": "val"}],
+    } in users
+
+    # Test skip arg
+    assert len(client.get("/api/users/", params={"skip": len(users) - 1}).json()) == 1
+
+    # Test limit arg
+    assert len(client.get("/api/users/", params={"limit": 1}).json()) == 1
