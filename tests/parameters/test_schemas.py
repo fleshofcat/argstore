f87d14731d727374586777b7ea086e280e342c96
fleshofcat <fleshofcat@gmail.com>
2022-09-17 12:09:56 +0300

    Add JsonApi schemas

--- tests/parameters/test_schemas.py
+++ tests/parameters/test_schemas.py
@@ -1,7 +1,7 @@
 import pytest
 from pydantic import ValidationError
 
-from argstore.parameters.schemas import Parameter
+from argstore.parameters.schemas import JsonApiQueryMessage, Parameter
 
 
 @pytest.mark.parametrize("name", ["string_name", "comb333name", "1234", "null"])
@@ -20,3 +20,36 @@ def test_parameter_schema_name_validation(bad_name):
 def test_parameter_schema_type_validation(bad_type):
     with pytest.raises(ValidationError):
         Parameter(**{"Name": "name", "Type": bad_type, "Value": "val"})
+
+
+@pytest.mark.parametrize(
+    "bad_part",
+    [
+        {"Operation": ""},
+        {"Operation": "."},
+        {"Operation": " "},
+        {"Operation": "unsupported_one"},
+        {"Name": ""},
+        {"Name": "name with space"},
+        {"Name": " "},
+        {"Name": "\t"},
+        {"Name": "name\twith\ttab"},
+        {"Type": ""},
+        {"Type": " "},
+        {"Type": "float"},
+        {"Type": "int", "Value": "text"},
+        {"Type": "int", "Value": ""},
+        {"Type": "int", "Value": " "},
+        {"Type": "int", "Value": "1.1"},
+    ],
+)
+def test_JsonApiQueryMessage_validation(bad_part: dict):
+    valid_json = {
+        "Operation": "SetParam",
+        "Name": "name",
+        "Type": "str",
+        "value": "1",
+    }
+
+    with pytest.raises(ValidationError):
+        JsonApiQueryMessage(**(valid_json | bad_part))
